f67331ce75f6944726eb7569d7ae2fc7757dea8e
nhk9710 <hknoh@wkit.co.kr>
2022-03-02 13:22:39 +0900

    [add]body half

--- js/index.js
+++ js/index.js
@@ -10,3 +10,13 @@
         prevArrow: $('.banner-prev'),
         nextArrow: $('.banner-next')
     });
+
+    $('.main-slick').slick({
+        slidesToShow:1,
+        slidesToScroll:1,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        prevArrow: $('.banner-prev'),
+        nextArrow: $('.banner-next')
+    });
