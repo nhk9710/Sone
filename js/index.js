58a6ca2deb6cc07027b8d8aa42be4d54168629f2
nhk9710 <hknoh@wkit.co.kr>
2022-03-03 13:14:40 +0900

    [add]All details

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
-
-    AOS.init();
-
-    $('.single-item').slick({
-        slidesToShow:1,
-        slidesToScroll:1,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        prevArrow: $('.banner-prev'),
-        nextArrow: $('.banner-next')
+$(document).ready(function() {
+    $('.fourth-slick').on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+        console.log(currentSlide);
+        const fourth_page = nextSlide+1;
+        document.querySelector('.fourth_current').textContent = '0'+fourth_page;
     });
+
+})
+    AOS.init();
 
     $('.main-slick').slick({
         slidesToShow:1,
@@ -18,5 +15,30 @@
         speed: 500,
         fade: true,
         prevArrow: $('.banner-prev'),
+        nextArrow: $('.banner-next'),
+    });
+
+
+$('.single-item').slick({
+        slidesToShow:1,
+        slidesToScroll:1,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        prevArrow: $('.banner-prev'),
         nextArrow: $('.banner-next')
     });
+
+
+    $('.fourth-slick').slick({
+        slidesToShow:1,
+        slidesToScroll:1,
+        infinite: true,
+        speed: 500,
+        vertical: true,
+        verticalSwiping: true,
+        prevArrow: $('.fourth-prev'),
+        nextArrow: $('.fourth-next')
+    });
+
+
